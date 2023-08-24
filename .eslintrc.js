257adb04214e211455f1b2309d250a27bf7deb8d
Henry Catalini Smith <henry@catalinismith.com>
2023-08-24 19:14:46 +0200

    db stuff

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  globals: {
+    process: true,
+    module: true,
+    Promise: true,
+    Buffer: true,
+    __DEV__: true,
+    __dirname: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'import'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'es5',
+      },
+    ],
+    'no-async-promise-executor': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    'react/prop-types': 0,
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  }
+}
---
+++ tasks/planetscale:dump.ts
@@ -0,0 +1,5 @@
+const { execSync } = require('child_process')
+
+execSync(
+  `pscale database dump ddosdotnews dev --output dumps/${new Date().toISOString()}`
+)
