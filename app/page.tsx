100bfa19681e81d7c21d429b12734ac8e272e4f6
Henry Catalini Smith <henry@catalinismith.com>
2023-08-25 22:24:51 +0200

    link

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   // <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   return (
     <div className="h-screen bg-snow w-full flex justify-center items-center">
-      <div className="w-40 h-40 bg-crimson"></div>
+      <a href="/companies/556737-0431">hello</a>
     </div>
   )
 }
