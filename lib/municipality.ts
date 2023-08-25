c0bbb21d565ae9ce55c967828c7f65ec65b41478
Henry Catalini Smith <henry@catalinismith.com>
2023-08-25 16:46:48 +0200

    document

--- lib/municipality.ts
+++ lib/municipality.ts
@@ -1,6 +1,23 @@
 import slugify from 'slugify'
 
-import { db } from '../lib/database'
+import { Municipality, db } from '../lib/database'
+
+export async function lookupMunicipality(m: string): Promise<Municipality> {
+  const matches = m.match(/(\d{4})/)
+  if (!matches) {
+    throw new Error(`No municipality ID found in "${m}"`)
+  }
+  const code = matches[1]
+  const municipality = await db
+    .selectFrom('municipality')
+    .selectAll()
+    .where('code', '=', code)
+    .executeTakeFirst()
+  if (!municipality) {
+    throw new Error(`No municipality found for ID "${code}"`)
+  }
+  return municipality as any as Municipality
+}
 
 export async function createMunicipality({
   name,
---
+++ migrations/20230825T134658-document.ts
@@ -0,0 +1,58 @@
+import { Kysely, sql } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('document')
+    .addColumn('id', 'serial', (col) => col.primaryKey())
+    // .addColumn('case_id', 'bigint', (col) => col.notNull())
+    .addColumn('company_id', 'bigint', (col) => col.notNull())
+    .addColumn('county_id', 'bigint', (col) => col.notNull())
+    .addColumn('municipality_id', 'bigint', (col) => col.notNull())
+    .addColumn('code', 'varchar(255)', (col) => col.notNull().unique())
+    .addColumn('type', 'varchar(255)', (col) => col.notNull())
+    .addColumn('cfar', 'varchar(255)', (col) => col.notNull())
+    .addColumn('workplace', 'varchar(255)', (col) => col.notNull())
+    .addColumn('direction', sql`enum('blank', 'incoming', 'outgoing')`, (col) =>
+      col.notNull()
+    )
+    .addColumn('status', sql`enum('ongoing', 'complete')`, (col) =>
+      col.notNull()
+    )
+    .addColumn('filed', 'date', (col) => col.notNull())
+    .addColumn('created', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn('updated', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
+    )
+    .addColumn('case_topic', 'varchar(255)', (col) => col.notNull())
+    .execute()
+
+  await db.schema
+    .createIndex('document_company_id')
+    .on('document')
+    .column('company_id')
+    .execute()
+
+  await db.schema
+    .createIndex('document_county_id')
+    .on('document')
+    .column('county_id')
+    .execute()
+
+  await db.schema
+    .createIndex('document_municipality_id')
+    .on('document')
+    .column('municipality_id')
+    .execute()
+
+  // await db.schema
+  //   .createIndex('document_case_id')
+  //   .on('document')
+  //   .column('case_id')
+  //   .execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('document').execute()
+}
