a356b94ca16118b7741564b4ade6574b78a81652
Henry Catalini Smith <henry@catalinismith.com>
2023-08-26 21:39:58 +0200

    remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import { cookies } from 'next/headers'
-
 import LoginForm from '../components/LoginForm'
 
 export default function Home() {
-  const cookieStore = cookies()
-
   return (
     <div className="h-screen bg-snow w-full flex justify-center items-center">
-      <p>{JSON.stringify(cookieStore.get('session'))}</p>
       <LoginForm />
     </div>
   )
