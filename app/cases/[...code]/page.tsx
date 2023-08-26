54015ec8d4514d8d24df8298e9e5ac4da6778e26
Henry Catalini Smith <henry@catalinismith.com>
2023-08-26 22:39:41 +0200

    fix

--- app/cases/[...code]/page.tsx
+++ app/cases/[...code]/page.tsx
@@ -21,38 +21,40 @@ export default async function Case({ params }: any) {
 
   return (
     <>
-      <div className="space-y-3">
-        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
-          {c!.code}
-        </h1>
-        <p className="text-lg text-muted-foreground">{c!.name}</p>
-      </div>
+      <div className="container pt-8">
+        <div className="space-y-3">
+          <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+            {c!.code}
+          </h1>
+          <p className="text-lg text-muted-foreground">{c!.name}</p>
+        </div>
 
-      <p className="pt-8">
-        <Link href={`/companies/${company.code}`}>{company.name}</Link>
-      </p>
+        <p className="pt-8">
+          <Link href={`/companies/${company.code}`}>{company.name}</Link>
+        </p>
 
-      <h2 className="pt-2 font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight">
-        Documents
-      </h2>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-left">Date</TableHead>
-            <TableHead className="text-left">Type</TableHead>
-            <TableHead className="text-left">Filing ID</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {documents.map((document: any) => (
-            <TableRow key={document.id}>
-              <TableCell>{document.filed}</TableCell>
-              <TableCell>{document.type}</TableCell>
-              <TableCell>{document.code}</TableCell>
+        <h2 className="pt-2 font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight">
+          Documents
+        </h2>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-left">Date</TableHead>
+              <TableHead className="text-left">Type</TableHead>
+              <TableHead className="text-left">Filing ID</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {documents.map((document: any) => (
+              <TableRow key={document.id}>
+                <TableCell>{document.filed}</TableCell>
+                <TableCell>{document.type}</TableCell>
+                <TableCell>{document.code}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </>
   )
 }
