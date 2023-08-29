ddcab04ad0026f593d78a03d2989e228429db7ab
Henry Catalini Smith <henry@catalinismith.com>
2023-08-29 14:47:07 +0200

    muni

--- prisma/migrations/20230829123633_company/migration.sql
+++ prisma/migrations/20230829123633_company/migration.sql
@@ -1,17 +1,17 @@
 -- CreateTable
-CREATE TABLE "county" (
-    "id" SERIAL NOT NULL,
+CREATE TABLE "company" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "code" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "county_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "company_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "county_code_key" ON "county"("code");
+CREATE UNIQUE INDEX "company_code_key" ON "company"("code");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "county_slug_key" ON "county"("slug");
+CREATE UNIQUE INDEX "company_slug_key" ON "company"("slug");
