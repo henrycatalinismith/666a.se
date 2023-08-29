7d3ce78166c722c61af611cf2e93cb3063ecc287
Henry Catalini Smith <henry@catalinismith.com>
2023-08-29 09:15:31 +0200

    county

---
+++ prisma/migrations/20230829071351_county/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "county" (
+    "id" SERIAL NOT NULL,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "county_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "county_code_key" ON "county"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "county_slug_key" ON "county"("slug");
--- tasks/county:import.ts
+++ tasks/county:import.ts
@@ -1,13 +1,17 @@
+import { PrismaClient } from '@prisma/client'
 import _ from 'lodash'
 
 import counties from '../data/county.json'
-import { createCounty } from '../lib/county'
+const prisma = new PrismaClient()
+
 ;(async () => {
   for (const county of _.sortBy(counties, 'name')) {
-    await createCounty({
-      name: county.name,
-      code: county.code,
-      slug: county.slug,
+    await prisma.county.create({
+      data: {
+        name: county.name,
+        code: county.code,
+        slug: county.slug,
+      },
     })
   }
 })()
