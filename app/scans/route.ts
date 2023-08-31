95d26a599be1c95254d496462d0967dbd5e3215b
Henry Catalini Smith <henry@catalinismith.com>
2023-08-31 22:12:53 +0200

    ingestion

---
+++ app/scans/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+
+import { requireUser } from '../../lib/authentication'
+import prisma from '../../lib/database'
+import { scanCounty } from '../../lib/ingestion'
+
+export async function POST(request: Request) {
+  const user = await requireUser()
+  if (!user) {
+    return NextResponse.json({ status: 'failure' })
+  }
+  console.log(user)
+
+  const body = await request.json()
+  const slug = body.county
+
+  const county = await prisma.county.findFirstOrThrow({
+    where: {
+      slug,
+    },
+  })
+
+  const scan = await scanCounty(county)
+
+  return NextResponse.json({
+    id: scan.id,
+  })
+}
---
+++ prisma/migrations/20230831193931_nullable/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "chunk" ALTER COLUMN "stubCount" DROP NOT NULL;
