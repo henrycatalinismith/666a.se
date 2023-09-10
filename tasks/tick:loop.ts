06f0c9e75fc54b70e3b3b3e600b0f13f10b6e434
Henry Catalini Smith <henry@catalinismith.com>
2023-09-10 09:30:48 +0200

    add tick:loop

---
+++ tasks/tick:loop.ts
@@ -0,0 +1,15 @@
+import { createTick } from '../lib/ingestion'
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+;(async () => {
+  for (let i = 0; i < 10000; i += 1) {
+    const tick = await createTick()
+    console.log(tick)
+    await sleep(1000)
+  }
+
+  process.exit(0)
+})()
