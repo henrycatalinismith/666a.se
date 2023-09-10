4136d42c94f025f6e9a9f50f80024832f9f1e616
Henry Catalini Smith <henry@catalinismith.com>
2023-09-10 10:51:54 +0200

    no rendering

--- app/scans/[id]/page.tsx
+++ app/scans/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { Relations } from 'components/Relations'
-import { ChunkIconDefinition } from 'icons/ChunkIcon'
 import { CountyIconDefinition } from 'icons/CountyIcon'
 import { ScanIconDefinition } from 'icons/ScanIcon'
 import { requireUser } from 'lib/authentication'
@@ -7,16 +6,7 @@ import prisma from 'lib/database'
 import _ from 'lodash'
 import { Card } from 'ui/Card'
 import { IconHeading } from 'ui/IconHeading'
-import { IconLink } from 'ui/IconLink'
 import { Progress } from 'ui/Progress'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from 'ui/Table'
 
 export default async function Document({ params }: any) {
   const user = await requireUser()
@@ -34,8 +24,6 @@ export default async function Document({ params }: any) {
 
   const totalChunks = scan.chunks.length
   const completeChunks = _.filter(scan.chunks, { status: 'SUCCESS' }).length
-  console.log(totalChunks)
-  console.log(completeChunks)
   const progress = (completeChunks / totalChunks) * 100
 
   return (
@@ -65,46 +53,6 @@ export default async function Document({ params }: any) {
             <Progress value={progress} />
           </Card>
         )}
-
-        <h2 className="font-heading scroll-m-20 text-xl font-semibold tracking-tight">
-          Chunks
-        </h2>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="text-left">ID</TableHead>
-              <TableHead className="text-left">Status</TableHead>
-              <TableHead className="text-left">Hit Count</TableHead>
-              <TableHead className="text-left">Stub Count</TableHead>
-              <TableHead className="text-left">Updated</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {scan.chunks.map((chunk) => (
-              <TableRow key={chunk.id}>
-                <TableCell>
-                  <IconLink
-                    icon={ChunkIconDefinition}
-                    href={`/chunks/${chunk.id}`}
-                  >
-                    {`${chunk.county.name} ${chunk.startDate
-                      ?.toISOString()
-                      .substring(0, 10)}`}
-                  </IconLink>
-                </TableCell>
-                <TableCell>{chunk.status}</TableCell>
-                <TableCell>{chunk.hitCount}</TableCell>
-                <TableCell>{chunk.stubCount}</TableCell>
-                <TableCell>
-                  {chunk.updated
-                    .toISOString()
-                    .substring(0, 19)
-                    .replace('T', ' ')}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
       </div>
     </>
   )
