0014690de4eaed49aa06a1e20b0a95bdb034fb0b
Henry Catalini Smith <henry@catalinismith.com>
2023-09-17 21:40:51 +0200

    add login page back

---
+++ app/[locale]/login/page.tsx
@@ -0,0 +1,9 @@
+import { Login } from 'forms/Login'
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen bg-snow w-full flex justify-center items-center">
+      <Login />
+    </div>
+  )
+}
