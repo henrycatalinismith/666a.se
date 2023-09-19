7164ce488138d60be8a432de3bb83d21d9fa1f90
Henry Catalini Smith <henry@catalinismith.com>
2023-09-19 21:18:20 +0200

    policies

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { NextIntlClientProvider } from 'next-intl'
 
@@ -53,7 +54,40 @@ export default async function LocaleLayout({
       <body>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NavBar links={links} />
-          <div className="flex flex-col">{children}</div>
+          <div className="flex flex-col min-h-screen">{children}</div>
+
+          <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+              <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                  <Link
+                    href="/policies/privacy-policy"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Privacy
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="/policies/accessibility-statement"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Accessibility
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="/policies/terms-of-service"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Terms of Service
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </footer>
         </NextIntlClientProvider>
       </body>
     </html>
