410ed1ae04db4b3be168f17c351313e7d0b73402
Henry Catalini Smith <henry@catalinismith.com>
2023-09-20 17:33:44 +0200

    add next-intl and tailwind config

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+import { NextIntlClientProvider } from 'next-intl'
+
+export function generateStaticParams() {
+  return [{ locale: 'en' }, { locale: 'de' }]
+}
+
+type LayoutProps = {
+  children: any
+  params: any
+}
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: LayoutProps) {
+  let messages: any
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default
+  } catch (error) {
+    notFound()
+  }
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <div className="flex flex-col min-h-screen">{children}</div>
+
+          <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+              <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                  <Link
+                    href="/policies/privacy-policy"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Privacy
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="/policies/accessibility-statement"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Accessibility
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="/policies/terms-of-service"
+                    className="mr-4 hover:underline md:mr-6"
+                  >
+                    Terms of Service
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </footer>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
+}
