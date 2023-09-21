eede6960f3acfa1af9170e883a7b90f444d721c1
Henry Catalini Smith <henry@catalinismith.com>
2023-09-21 19:25:23 +0200

    alway

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from 'next-intl/middleware'
 export default createMiddleware({
   locales: ['en', 'sv'],
   defaultLocale: 'sv',
+  localePrefix: 'always',
 })
 
 export const config = {
