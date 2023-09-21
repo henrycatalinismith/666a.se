5068227f70b9c4e989c8cc8c6690d00c798c0aa1
Henry Catalini Smith <henry@catalinismith.com>
2023-09-21 19:45:10 +0200

    components

---
+++ components/SendButton.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { FC } from 'react'
+
+import { Button } from 'ui/Button'
+
+export type SendButtonProps = {
+  id: string
+}
+
+export const SendButton: FC<SendButtonProps> = ({ id }) => {
+  const onSend = async () => {
+    const params = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    }
+    const response = await fetch(`/api/notifications/${id}/send`, params).then(
+      (response) => response.json()
+    )
+    console.log(response)
+  }
+  return (
+    <>
+      <Button onClick={onSend}>Send</Button>
+    </>
+  )
+}
