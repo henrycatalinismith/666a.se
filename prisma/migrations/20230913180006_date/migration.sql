9c67a2c813e66930f0b1b135d8195b9da9dacac9
Henry Catalini Smith <henry@catalinismith.com>
2023-09-24 09:15:45 +0200

    reinit

--- prisma/migrations/20230913180006_date/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Scan" DROP CONSTRAINT "Scan_countyId_fkey";
-
--- AlterTable
-ALTER TABLE "Scan" ADD COLUMN     "date" DATE,
-ALTER COLUMN "countyId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Scan" ADD CONSTRAINT "Scan_countyId_fkey" FOREIGN KEY ("countyId") REFERENCES "County"("id") ON DELETE SET NULL ON UPDATE CASCADE;
