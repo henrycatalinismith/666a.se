80ae4bd5a1ed514f48273016aaeb07a5efe42560
Henry Catalini Smith <henry@catalinismith.com>
2023-09-25 21:07:58 +0200

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+  devise_for :users,
+    :path => '',
+    :path_names => {
+      :sign_in => "login",
+      :sign_out => "logout",
+      :sign_up => "register",
+    }
 end
