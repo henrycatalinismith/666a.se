6059d0578a5e3bb11bba6a567483f1da95458656
Henry Catalini Smith <henry@catalinismith.com>
2023-09-25 20:18:43 +0200

    rails g controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
+# https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
 end
