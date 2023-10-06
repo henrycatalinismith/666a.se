ce550a6b13131d1dee0d2af44b5330d0e4db39ed
Henry Catalini Smith <henry@catalinismith.com>
2023-10-06 22:30:11 +0200

    fix

--- db/migrate/20231006184645_role_props.rb
+++ db/migrate/20231006184645_role_props.rb
@@ -1,7 +1,6 @@
 class RoleProps < ActiveRecord::Migration[7.1]
   def change
     change_table :roles do |t|
-      t.references :user, null: false, type: :string, foreign_key: true
     end
   end
 end
