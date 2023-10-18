531eed412cc3c0876aed20671b2e889fd9b7b81d
Henry Catalini Smith <henry@catalinismith.com>
2023-10-18 20:47:40 +0200

    whatev

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,11 @@ class AdminController < ApplicationController
   layout "internal"
 
   def index
+    @days = Day.reverse_chronological
+  end
+
+  def day
+    @day = Day.find_by(date: params[:date])
   end
 
   def stats
