c11aeacec18610577c8c5142df82e67418e0a6a6
Henry Catalini Smith <henry@catalinismith.com>
2023-10-20 12:37:53 +0200

    schedule backfill

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,9 +5,17 @@ require "rufus-scheduler"
 return if defined?(Rails::Console) || Rails.env.test? || File.split($PROGRAM_NAME).last == 'rake'
 
 scheduler = Rufus::Scheduler.singleton
+
 scheduler.cron("0 9 * * *") do
   MorningJob.perform_later(Date.yesterday.strftime("%Y-%m-%d"), {
     :cascade => true,
     :notify => true,
+  })
+end
+
+scheduler.cron("45 10 * * *") do
+  BackfillJob.perform_later({
+    :cascade => true,
+    :notify => false,
   })
 end
