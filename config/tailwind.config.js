5eef9e72f30c74351c6698f7358e2a00217b8735
Henry Catalini Smith <henry@catalinismith.com>
2023-10-24 18:34:55 +0200

    please

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -28,10 +28,10 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('flowbite/plugin'),
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/container-queries'),
+    // require('flowbite/plugin'),
   ]
 }
