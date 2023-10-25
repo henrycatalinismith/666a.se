449b5b8c2f70feea858da4d48bd1fc77e9b1332f
Henry Catalini Smith <henry@catalinismith.com>
2023-10-25 14:30:06 +0200

    fix emails for workplace subs

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,7 @@ class NotificationMailer < ApplicationMailer
   def notification_email()
     @notification = params[:notification]
     @document = @notification.document
+    @target = @notification.subscription.company_subscription? ? @document.company_name : @document.workplace_name
     @user = @notification.subscription.user
     @url = "https://www.av.se/om-oss/sok-i-arbetsmiljoverkets-diarium/?id=" + @document.document_code
     @unsubscribe_url = "https://666a.se/unsubscribe/#{@notification.subscription.id}"
