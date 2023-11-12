a3f8a517d474930669e9ad6f9b57f9c9353779ae
Henry Catalini Smith <henry@catalinismith.com>
2023-11-12 08:27:56 +0100

    tidy up

--- app/helpers/legal/revisions_helper.rb
+++ app/helpers/legal/revisions_helper.rb
@@ -1,2 +1,13 @@
 module Legal::RevisionsHelper
+  def both_sides()
+    "max-w-[50%] overflow-hidden"
+  end
+
+  def left_classes()
+    "pr-1 md:pr-4"
+  end
+
+  def right_classes()
+    "pl-1 md:pl-4"
+  end
 end
