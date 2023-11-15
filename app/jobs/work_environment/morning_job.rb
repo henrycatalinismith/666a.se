8a237ba957f99da562b8a3e3bf18152493aa9485
Henry Catalini Smith <henry@catalinismith.com>
2023-11-15 06:10:33 +0100

    Fix

--- app/jobs/work_environment/morning_job.rb
+++ app/jobs/work_environment/morning_job.rb
@@ -4,8 +4,10 @@ class WorkEnvironment::MorningJob < ApplicationJob
   def perform(date, options = {})
     day = Period::Day.find_by(date:)
     if day.nil? then
-      Period::CreateDayJob.perform_now(date)
-      day = Period::Day.find_by(date: date)
+      day = Period::Day.create(
+        date: date,
+        ingestion_status: :ingestion_pending,
+      )
       day.ingestion_active!
     end
 
