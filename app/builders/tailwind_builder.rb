5c56f48ea36a970192d99e5baf4cff183cbd1811
Henry Catalini Smith <henry@catalinismith.com>
2023-12-05 13:13:00 +0100

    taiwindui

--- app/builders/tailwind_builder.rb
+++ app/builders/tailwind_builder.rb
@@ -12,28 +12,31 @@ class TailwindBuilder < ActionView::Helpers::FormBuilder
   end
 
   def text_field(attribute, options={})
-    super(attribute, options.reverse_merge(class: "border py-2 px-3 block input border-gray-600 rounded-md shadow-sm focus:ring focus:ring-indigo-200 flex-1 w-full"))
+    super(attribute, options.reverse_merge(class: "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"))
   end
 
   def select(method, choices = nil, options = {}, html_options = {}, &block)
-    super(method, choices, options, html_options.merge(class: "border py-2 px-3 block input border-gray-600 rounded-md shadow-sm focus:ring focus:ring-indigo-200 f min-w-full"), &block)
+    super(method, choices, options, html_options.merge(class: "mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"), &block)
   end
 
   def email_field(attribute, options={})
-    super(attribute, options.reverse_merge(class: "border py-2 px-3 block input border-gray-600 rounded-md shadow-sm focus:ring focus:ring-indigo-200 w-full"))
+    super(attribute, options.reverse_merge(class: "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"))
   end
 
   def password_field(attribute, options={})
-    super(attribute, options.reverse_merge(class: "border py-2 px-3 block input border-gray-600 rounded-md shadow-sm focus:ring focus:ring-indigo-200 w-full"))
+    super(attribute, options.reverse_merge(class: "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"))
   end
 
   def label(attribute, options={})
-    super(attribute, options.reverse_merge(class: "font-bold"))
+    super(attribute, options.reverse_merge(class: "block text-sm font-medium leading-6 text-gray-900"))
   end
 
   def submit(attribute, options={})
-    super(attribute, options.reverse_merge(class: "bg-cyan-300 hover:bg-cyan-200 text-black font-bold py-2 px-4 rounded"))
+    super(attribute, options.reverse_merge(class: "rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"))
     # bg-cyan-300 hover:bg-cyan-200 text-black font-bold 
   end
 
+  def check_box(attribute, options={})
+    super(attribute, options.reverse_merge(class: "h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-60 ms-1"))
+  end
 end
