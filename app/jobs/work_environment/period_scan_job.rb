0c4b945a1f982d91eed21869604115f5811bc7e8
Henry Catalini Smith <henry@catalinismith.com>
2024-01-08 06:11:06 +0100

    PeriodScan

---
+++ app/jobs/work_environment/period_scan_job.rb
@@ -0,0 +1,27 @@
+require "time"
+
+class WorkEnvironment::PeriodScanJob < ApplicationJob
+  queue_as :default
+
+  def perform(start, finish)
+    puts "PeriodScanJob: begin"
+
+    day = Period::Day.find_by(date: start)
+
+    puts day.inspect
+    if day.nil? then
+      return
+    end
+
+    while day.date <= Date.parse(finish) do 
+      WorkEnvironment::SearchJob.set(wait: 1.seconds).perform_later(day.date, {
+        :cascade => false,
+        :notify => false,
+        :force => false,
+      })
+      sleep 2.seconds
+    end
+
+    puts "PeriodScanJob: end"
+  end
+end
