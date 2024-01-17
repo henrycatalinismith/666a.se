2f245dd740ed89d68b315d099b7eff528ab87900
Henry Catalini Smith <henry@catalinismith.com>
2024-01-17 12:35:22 +0100

    Fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-
 class ActiveSupport::TestCase
   parallelize(workers: 2)
 end
