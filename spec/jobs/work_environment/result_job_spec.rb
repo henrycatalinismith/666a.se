a835ade0485b0cb5797354107d130dfadeb25904
Henry Catalini Smith <henry@catalinismith.com>
2024-02-02 21:07:51 +0100

    result test

---
+++ spec/jobs/work_environment/result_job_spec.rb
@@ -0,0 +1,55 @@
+require "rails_helper"
+
+describe WorkEnvironment::ResultJob do
+  let(:week) { TimePeriod::Week.new(week_code: "2023-W44") }
+
+  let(:day) do
+    TimePeriod::Day.new(
+      week:,
+      date: "2023-10-31",
+      ingestion_status: :ingestion_pending
+    )
+  end
+
+  let(:search) do
+    WorkEnvironment::Search.new(
+      day: day,
+      result_status: :result_pending,
+      page_number: 1
+    )
+  end
+
+  let(:result) do
+    WorkEnvironment::Result.new(search: search, document_code: "2023/018014-1")
+  end
+
+  let(:response) do
+    File.read("spec/fixtures/work_environment/result/2023-018014-1.html")
+  end
+
+  it "performs the job" do
+    search.save
+    result.save
+
+    stub_request(
+      :get,
+      "https://www.av.se/om-oss/sok-i-arbetsmiljoverkets-diarium/?id=2023%2F018014-1"
+    ).to_return(status: 200, body: response)
+
+    WorkEnvironment::ResultJob.perform_now(result.document_code)
+
+    result.reload
+
+    expect(result.case_code).to eq("2023/018014")
+    expect(result.case_date).to eq("2023-03-23")
+    expect(result.case_status).to eq(:case_concluded)
+    expect(result.company_code).to eq(nil)
+    expect(result.company_name).to eq(nil)
+    expect(result.workplace_code).to eq(nil)
+    expect(result.workplace_name).to eq(nil)
+    expect(result.county_code).to eq(nil)
+    expect(result.county_name).to eq(nil)
+    expect(result.municipality_code).to eq(nil)
+    expect(result.municipality_name).to eq(nil)
+  end
+end
