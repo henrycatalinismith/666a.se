80130d7ad78fec9451da5709aaec4e36c05a5ad2
Henry Catalini Smith <henry@catalinismith.com>
2024-02-02 20:30:06 +0100

    add unit test

--- app/jobs/work_environment/search_job.rb
+++ app/jobs/work_environment/search_job.rb
@@ -9,32 +9,33 @@ class WorkEnvironment::SearchJob < ApplicationJob
 
   def perform(date, options = {})
     day = TimePeriod::Day.find_by(date: date)
-    if day.nil? then
-      return
-    end
+    return if day.nil?
 
-    @search = day.searches.create(
-      result_status: :result_pending,
-      page_number: day.next_page_number,
-    )
+    @search =
+      day.searches.create(
+        result_status: :result_pending,
+        page_number: day.next_page_number
+      )
 
     @search.result_fetching!
-    puts @search.url
+    # puts @search.url
     uri = URI(@search.url)
     response = Net::HTTP.get_response(uri)
     document = Nokogiri::HTML.parse(response.body)
 
     hit_count = document.css(".hit-count").text.strip
-    headings = document.css("#handling-results thead th").map { |e| e.text.strip }
-    rows = document
-      .css("#handling-results tbody tr")
-      .map { |e| e.css("td").map { |e| e.text.strip } }
-      .map { |r| Hash[headings.zip(r)] }
+    headings =
+      document.css("#handling-results thead th").map { |e| e.text.strip }
+    rows =
+      document
+        .css("#handling-results tbody tr")
+        .map { |e| e.css("td").map { |e| e.text.strip } }
+        .map { |r| Hash[headings.zip(r)] }
 
     @search.hit_count = hit_count
 
     rows.each do |row|
-      document_code = row['Handlingsnummer']
+      document_code = row["Handlingsnummer"]
       document = WorkEnvironment::Document.find_by(document_code:)
       document_exists = !document.nil?
       document_status = document_exists ? :document_ready : :document_pending
@@ -43,20 +44,23 @@ class WorkEnvironment::SearchJob < ApplicationJob
         metadata_status:,
         document_status:,
         document_code:,
-        case_name: row['Ärendemening'],
-        document_type: row['Handlingstyp'],
-        document_date: row['Datum'],
-        organisation_name: row['Organisation'],
-        metadata: "",
+        case_name: row["Ärendemening"],
+        document_type: row["Handlingstyp"],
+        document_date: row["Datum"],
+        organisation_name: row["Organisation"],
+        metadata: ""
       )
     end
 
     @search.result_ready!
 
-    if options[:cascade] then
+    if options[:cascade]
       @search.results.each_with_index do |result, index|
-        if result.metadata_pending? then
-          WorkEnvironment::ResultJob.set(wait: index.seconds).perform_later(result.document_code, options)
+        if result.metadata_pending?
+          WorkEnvironment::ResultJob.set(wait: index.seconds).perform_later(
+            result.document_code,
+            options
+          )
         end
       end
     end
