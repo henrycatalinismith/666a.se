0da2f1557773a3e6ccf9d2af8af37851c433e08b
Henry Catalini Smith <henry@catalinismith.com>
2024-02-28 19:26:28 +0100

    add emails per week stats

--- app/models/time_period/week.rb
+++ app/models/time_period/week.rb
@@ -3,4 +3,5 @@ class TimePeriod::Week < ApplicationRecord
 
   scope :chronological, -> { order(week_code: :asc) }
   scope :reverse_chronological, -> { order(week_code: :desc) }
+  scope :since_launch, -> { where("week_code >= ?", "2023-W44") }
 end
