855903858ddce93f63a673f09417ec897f67e3fc
Henry Catalini Smith <henry@catalinismith.com>
2024-03-01 16:47:05 +0100

    Scan back 64 days

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -204,30 +204,254 @@ scheduler.cron("10 15 * * *") do
   )
 end
 
-scheduler.cron("20 16 * * *") do
+scheduler.cron("20 15 * * *") do
   WorkEnvironment::DayJob.perform_later(
     29.days.ago.strftime("%Y-%m-%d"),
     { cascade: true, force: true, notify: true }
   )
 end
 
-scheduler.cron("30 16 * * *") do
+scheduler.cron("30 15 * * *") do
   WorkEnvironment::DayJob.perform_later(
     30.days.ago.strftime("%Y-%m-%d"),
     { cascade: true, force: true, notify: true }
   )
 end
 
-scheduler.cron("40 16 * * *") do
+scheduler.cron("40 15 * * *") do
   WorkEnvironment::DayJob.perform_later(
     31.days.ago.strftime("%Y-%m-%d"),
     { cascade: true, force: true, notify: true }
   )
 end
 
-scheduler.cron("50 16 * * *") do
+scheduler.cron("50 15 * * *") do
   WorkEnvironment::DayJob.perform_later(
     32.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    33.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    34.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("20 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    35.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("30 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    36.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("40 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    37.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("50 16 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    38.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    39.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    40.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("20 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    41.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("30 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    42.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("40 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    43.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("50 17 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    44.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    45.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    46.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("20 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    47.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("30 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    48.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("40 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    49.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("50 18 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    50.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    51.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    52.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("20 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    53.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("30 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    54.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("40 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    55.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("50 19 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    56.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    57.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    58.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("20 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    59.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("30 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    60.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("40 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    61.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("50 20 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    62.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("0 21 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    63.days.ago.strftime("%Y-%m-%d"),
+    { cascade: true, force: true, notify: true }
+  )
+end
+
+scheduler.cron("10 21 * * *") do
+  WorkEnvironment::DayJob.perform_later(
+    64.days.ago.strftime("%Y-%m-%d"),
     { cascade: true, force: true, notify: true }
   )
 end
