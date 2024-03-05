03469b62daecbb09b283428ca68790aa1b7efc87
Henry Catalini Smith <henry@catalinismith.com>
2024-03-05 19:35:42 +0100

    Purge cached searches and results before scanning

--- app/controllers/admin/time_period/days_controller.rb
+++ app/controllers/admin/time_period/days_controller.rb
@@ -11,7 +11,13 @@ class Admin::TimePeriod::DaysController < AdminController
 
   def job
     @day = TimePeriod::Day.find(params[:id])
-    WorkEnvironment::DayJob.perform_later(@day.date, cascade: true, force: true, notify: true)
+    WorkEnvironment::DayJob.perform_later(
+      @day.date,
+      cascade: true,
+      force: true,
+      notify: true,
+      purge: true
+    )
     redirect_to "/admin/time_period/days/#{@day.id}"
     flash[:notice] = "job queued"
   end
