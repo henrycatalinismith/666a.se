5f4a7bccc22237be99ad69d3f9afb3b726c5cf59
Henry Catalini Smith <henry@catalinismith.com>
2024-04-04 20:20:09 +0200

    Fix path

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ git pull origin main
 git reset --hard origin/main
 
 # Copy the launch agent to the LaunchDaemons directory
-cp se.666a.backup.plist ~/Library/LaunchDaemons/se.666a.backup.plist
+cp se.666a.backup.plist ~/Library/LaunchAgents/se.666a.backup.plist
 
 # Reload se.666a.backup.plist so that the new version of the launch agent is loaded
-launchctl unload ~/Library/LaunchDaemons/se.666a.backup.plist
-launchctl load ~/Library/LaunchDaemons/se.666a.backup.plist
+launchctl unload ~/Library/LaunchAgents/se.666a.backup.plist
+launchctl load ~/Library/LaunchAgents/se.666a.backup.plist
