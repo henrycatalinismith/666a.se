b8426ec80c9ba33ef1be31db9d199191752f4189
Henry Catalini Smith <henry@catalinismith.com>
2024-04-05 20:21:54 +0200

    Get backup working as a shell script

---
+++ backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+fly -a sixa sftp get /data/production/data.sqlite3
+fly -a sixa sftp get /data/production/data.sqlite3-shm
+fly -a sixa sftp get /data/production/data.sqlite3-wal
+
+timestamp=$(date +"%Y%m%d.%H%M%S")
+zip -r "db.${timestamp}.zip" data.sqlite3 data.sqlite3-shm data.sqlite3-wal
+
+for zip_file in db.*.zip; do
+  mv "$zip_file" "/Volumes/Samsung/666a"
+done
+
+files=(/Volumes/Samsung/666a/*)
+if [ ${#files[@]} -gt 4 ]; then
+  oldest_file=$(ls -t /Volumes/Samsung/666a/* | tail -1)
+  rm "$oldest_file"
+fi
+
+rm data.sqlite3
+rm data.sqlite3-shm
+rm data.sqlite3-wal
+
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-cd /Users/henrycatalinismith/666a.se/launch-agents
-git pull origin main
-git reset --hard origin/main
+# cd /Users/henrycatalinismith/666a.se/launch-agents
+# git pull origin main
+# git reset --hard origin/main
 
 # Copy the launch agent to the LaunchDaemons directory
 cp se.666a.backup.plist ~/Library/LaunchAgents/se.666a.backup.plist
 
 # Reload se.666a.backup.plist so that the new version of the launch agent is loaded
-launchctl unload ~/Library/LaunchAgents/se.666a.backup.plist
-launchctl load ~/Library/LaunchAgents/se.666a.backup.plist
+launchctl bootout gui/501/se.666a.backup
+launchctl bootstrap gui/501 /Users/henrycatalinismith/Library/LaunchAgents/se.666a.backup.plist
+launchctl enable gui/501/se.666a.backup
