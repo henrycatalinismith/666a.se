da13d3263a24e2a8a417d5a6062e7ea72643c362
Henry Catalini Smith <henry@catalinismith.com>
2024-04-11 17:10:36 +0200

    Add Discord integration

---
+++ config/initializers/discord.rb
@@ -0,0 +1,22 @@
+require "discordrb"
+
+bot = Discordrb::Bot.new token: ENV["DISCORD_BOT_AUTH_TOKEN"]
+
+bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server starting`")
+
+at_exit do
+  bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server stopping`")
+end
+
+jobs_to_notify = [
+  WorkEnvironment::MorningJob,
+  WorkEnvironment::DayJob,
+  WorkEnvironment::SearchJob,
+  WorkEnvironment::ResultJob,
+]
+
+ActiveSupport::Notifications.subscribe("perform.active_job") do |event|
+  if jobs_to_notify.include?(event.payload[:job].class) then
+    bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`#{event.payload[:job].class} #{event.payload[:job].arguments.first}`")
+  end
+end
