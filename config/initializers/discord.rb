98b7fbc5d50b60e39c8f8acbc20acb65e6b93c74
Henry Catalini Smith <henry@catalinismith.com>
2024-04-16 19:43:18 +0200

    Block Discord in dev

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,7 +1,7 @@
 require "discordrb"
 
 if !ENV["DISCORD_BOT_AUTH_TOKEN"].nil? and
-     !ENV["DISCORD_BOT_LOG_CHANNEL_ID"].nil?
+     !ENV["DISCORD_BOT_LOG_CHANNEL_ID"].nil? and Rails.env.production?
   bot = Discordrb::Bot.new token: ENV["DISCORD_BOT_AUTH_TOKEN"]
 
   #bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server starting`")
