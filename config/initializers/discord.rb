ab64f07aaa1e8cdca3abfeca838dfe8a5ba71ccd
Henry Catalini Smith <henry@catalinismith.com>
2024-04-16 19:25:03 +0200

    Less Discord logging for now

--- config/initializers/discord.rb
+++ config/initializers/discord.rb
@@ -1,21 +1,21 @@
 require "discordrb"
 
-if !ENV["DISCORD_BOT_AUTH_TOKEN"].nil? and !ENV["DISCORD_BOT_LOG_CHANNEL_ID"].nil? then
-
+if !ENV["DISCORD_BOT_AUTH_TOKEN"].nil? and
+     !ENV["DISCORD_BOT_LOG_CHANNEL_ID"].nil?
   bot = Discordrb::Bot.new token: ENV["DISCORD_BOT_AUTH_TOKEN"]
 
   #bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server starting`")
 
   #at_exit do
-    #bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server stopping`")
+  #bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`rails server stopping`")
   #end
 
   ActiveSupport::Notifications.subscribe("perform.active_job") do |event|
-    if event.payload[:job].class == WorkEnvironment::MorningJob or 
-      event.payload[:job].class == WorkEnvironment::DayJob or
-      event.payload[:job].class == WorkEnvironment::SearchJob or
-      event.payload[:job].class == WorkEnvironment::ResultJob then
-      bot.send_message(ENV["DISCORD_BOT_LOG_CHANNEL_ID"], "`#{event.payload[:job].class} #{event.payload[:job].arguments.first}`")
+    if event.payload[:job].class == WorkEnvironment::DayJob
+      bot.send_message(
+        ENV["DISCORD_BOT_LOG_CHANNEL_ID"],
+        "`#{event.payload[:job].class} #{event.payload[:job].arguments.first}`"
+      )
     end
   end
 end
