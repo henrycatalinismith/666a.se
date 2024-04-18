e20cdd490d0bbd890c9a4e4385744e4c737e0aac
Henry Catalini Smith <henry@catalinismith.com>
2024-04-18 07:26:08 +0200

    Fix layout and try to fix JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,11 @@
-const burger = document.querySelector('[aria-controls="mobile-menu"]');
-const menu = document.querySelector('#mobile-menu');
+console.log('⛧')
 
-burger.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-  burger.setAttribute('aria-expanded', menu.classList.contains('hidden') ? 'false' : 'true');
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('[aria-controls="mobile-menu"]')
+  const menu = document.querySelector('#mobile-menu')
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+    burger.setAttribute('aria-expanded', menu.classList.contains('hidden') ? 'false' : 'true')
+  })
+})
