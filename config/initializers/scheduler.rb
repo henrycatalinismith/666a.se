419b6b8522fc56e349a14f0a21bad3f45f40de09
Henry Catalini Smith <henry@catalinismith.com>
2024-04-18 07:58:16 +0200

    No scheduler in dev

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,7 +3,7 @@ require "rufus-scheduler"
 # do not schedule when Rails is run from its console, for a test/spec, or from a
 # Rake task
 if defined?(Rails::Console) || Rails.env.test? ||
-     File.split($PROGRAM_NAME).last == "rake"
+     File.split($PROGRAM_NAME).last == "rake" || Rails.env.development?
   return
 end
 
