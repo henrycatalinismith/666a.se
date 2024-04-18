cd416c7a4df607aea1e26203a6587c047db283f9
Henry Catalini Smith <henry@catalinismith.com>
2024-04-18 07:50:04 +0200

    Footer on every page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def h1(&block)
     "<h1 class=\"text-lg md:text-3xl font-bold\">#{capture(&block)}</h1>".html_safe
   end
+
+  def policies
+    [
+      { name: "Accessibility", slug: "accessibility" },
+      { name: "Privacy Policy", slug: "privacy" },
+      { name: "Terms of Service", slug: "terms" }
+    ]
+  end
 end
