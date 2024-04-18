e1ada5da7802723753c645d635e9f659189cb529
Henry Catalini Smith <henry@catalinismith.com>
2024-04-18 07:45:21 +0200

    Move it to /public. Bloody hell importmaps are annoying

---
+++ public/menu.js
@@ -0,0 +1,11 @@
+console.log('⛧')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('[aria-controls="mobile-menu"]')
+  const menu = document.querySelector('#mobile-menu')
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+    burger.setAttribute('aria-expanded', menu.classList.contains('hidden') ? 'false' : 'true')
+  })
+})
