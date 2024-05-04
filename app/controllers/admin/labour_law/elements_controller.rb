56a70deb912670eabac6e21e6862ac92cbd82137
Henry Catalini Smith <henry@catalinismith.com>
2024-05-04 13:32:21 +0200

    Add admin UI around new chapter/section/paragraph fields

--- app/controllers/admin/labour_law/elements_controller.rb
+++ app/controllers/admin/labour_law/elements_controller.rb
@@ -1,20 +1,23 @@
 class Admin::LabourLaw::ElementsController < AdminController
   layout "internal"
 
-  def edit
-    @document = LabourLaw::Document.find_by(document_code: params[:document_code])
-    @revision = @document.revisions.find_by(revision_code: params[:revision_code])
-    @element = @revision.elements.find_by(element_code: params[:element_code])
-    if request.patch? then
-      if @element.update(params[:element].permit(:element_type, :element_index, :element_code, :element_text))
-        flash[:notice] = "element updated"
-      end
-    end
+  def show
+    @element = LabourLaw::Element.find(params[:id])
+    @revision = @element.revision
+    @document = @revision.document
   end
 
   def update
     @element = LabourLaw::Element.find(params[:id])
-    if @element.update(params[:element].permit(:element_type, :element_index, :element_code, :element_text))
+    if @element.update(params[:element].permit(
+      :element_type,
+      :element_index,
+      :element_code,
+      :element_text,
+      :element_chapter,
+      :element_section,
+      :element_paragraph,
+      ))
       flash[:notice] = "element updated"
       redirect_to "/admin/labour_law/elements/#{@element.id}"
     end
@@ -40,39 +43,8 @@ class Admin::LabourLaw::ElementsController < AdminController
     if @revision.elements.count == 0 then
       return
     elsif type == "new_paragraph" then
-      npmatch = @prev.element_code.match(/\AK(\d+)P(\d+)/)
-      if !npmatch.nil? then
-        @element.element_type = "h3"
-        @element.element_code = "K#{npmatch[1]}P#{npmatch[2].to_i + 1}"
-        @element.element_text = "#{npmatch[2].to_i + 1} §"
-      end
-      npmatch = @prev.element_code.match(/\AP(\d+)/)
-      if !npmatch.nil? then
-        @element.element_type = "h3"
-        @element.element_code = "P#{npmatch[1].to_i + 1}"
-        @element.element_text = "#{npmatch[1].to_i + 1} §"
-      end
-    elsif @prev.element_code.match(/\AK+\dP\d+\Z/) then
-      @element.element_type = "md"
-      @element.element_code = "#{@prev.element_code}S1"
-    elsif @prev.element_code.match(/\AP\d+\Z/) then
-      @element.element_type = "md"
-      @element.element_code = "#{@prev.element_code}S1"
-    elsif @prev.element_code.match(/\AP\d+[a-z]\Z/) then
-      @element.element_type = "md"
-      @element.element_code = "#{@prev.element_code}S1"
-    elsif @prev.element_code.match(/\AK\d+P\d+S\d+\Z/) then
-      smatch = @prev.element_code.match(/\A(K\d+P\d+S)(\d+)\Z/)
-      @element.element_type = "md"
-      @element.element_code = "#{smatch[1]}#{smatch[2].to_i+1}"
-    elsif @prev.element_code.match(/\AP\d+S\d+\Z/) then
-      smatch = @prev.element_code.match(/\A(P\d+S)(\d+)\Z/)
-      @element.element_type = "md"
-      @element.element_code = "#{smatch[1]}#{smatch[2].to_i+1}"
-    elsif @prev.element_code.match(/\AP\d+[a-z]S\d+\Z/) then
-      smatch = @prev.element_code.match(/\A(P\d+[a-z]S)(\d+)\Z/)
-      @element.element_type = "md"
-      @element.element_code = "#{smatch[1]}#{smatch[2].to_i+1}"
+      @element.element_chapter = @prev.element_chapter
+      @element.element_section = @prev.element_section
     end
   end
 
@@ -81,7 +53,15 @@ class Admin::LabourLaw::ElementsController < AdminController
     if @revision.nil?
       raise ActionController::RoutingError.new("Not Found")
     end
-    @element = @revision.elements.new(params[:element].permit(:element_type, :element_index, :element_code, :element_text))
+    @element = @revision.elements.new(params[:element].permit(
+      :element_type,
+      :element_index,
+      :element_code,
+      :element_text,
+      :element_chapter,
+      :element_section,
+      :element_paragraph,
+    ))
     pmatch = @element.element_code.match(/P(\d+)\Z/)
     if pmatch and @element.element_text.empty? then
       @element.element_text = "#{pmatch[1]} §"
@@ -105,20 +85,8 @@ class Admin::LabourLaw::ElementsController < AdminController
         translation_text: "",
       )
 
-      if @element.element_type == "h3" and @element.element_code.match(/P\d/) then
-        match = @element.element_text.match(/\A(\d+) ([a-z])?/)
-        english = "Section #{match[1]}#{match[2]}"
-        @element.translations.first.update(translation_text: english)
-      end
-
       redirect_to "/admin/labour_law/elements/#{@element.id}"
       flash[:notice] = "element created"
     end
-  end
-
-  def show
-    @element = LabourLaw::Element.find(params[:id])
-    @revision = @element.revision
-    @document = @revision.document
   end
 end
