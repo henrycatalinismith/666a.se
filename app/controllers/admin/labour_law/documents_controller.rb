d9641fff2a81b6dedd79867b46ace93c9576e689
Henry Catalini Smith <henry@catalinismith.com>
2024-05-05 10:18:37 +0200

    Reduce weirdness of document admin

--- app/controllers/admin/labour_law/documents_controller.rb
+++ app/controllers/admin/labour_law/documents_controller.rb
@@ -5,21 +5,25 @@ class Admin::LabourLaw::DocumentsController < AdminController
     @documents = LabourLaw::Document.all
   end
 
+  def show
+    @document = LabourLaw::Document.find(params[:id])
+    if @document.nil?
+      raise ActionController::RoutingError.new("Not Found")
+    end
+  end
+
+  def edit
+    @document = LabourLaw::Document.find(params[:id])
+  end
+
   def update
-    @document = LabourLaw::Document.find_by(document_code: params[:id])
+    @document = LabourLaw::Document.find(params[:id])
     if @document.nil?
       raise ActionController::RoutingError.new("Not Found")
     end
     if @document.update(params[:document].permit(:document_name, :document_code)) then
-      redirect_to "/admin/labour_law/documents/#{@document.document_code}"
+      redirect_to admin_labour_law_document_path(@document)
       flash[:notice] = "document updated"
-    end
-  end
-
-  def show
-    @document = LabourLaw::Document.find_by(document_code: params[:id])
-    if @document.nil?
-      raise ActionController::RoutingError.new("Not Found")
     end
   end
 
@@ -28,9 +32,12 @@ class Admin::LabourLaw::DocumentsController < AdminController
   end
 
   def create
-    @document = LabourLaw::Document.create(params[:document].permit(:document_name, :document_code))
+    @document = LabourLaw::Document.create(params[:document].permit(
+      :document_name,
+      :document_code,
+    ))
     if @document.valid? then
-      redirect_to "/admin/labour_law/documents/#{@document.document_code}"
+      redirect_to "/admin/labour_law/documents/#{@document.id}"
       flash[:notice] = "document created"
     end
   end
