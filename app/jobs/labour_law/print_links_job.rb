dd0cbef4b188352f5f701ab6b5688ccfa224033a
Henry Catalini Smith <henry@catalinismith.com>
2024-06-02 07:34:17 +0200

    Tweak link printer

--- app/jobs/labour_law/print_links_job.rb
+++ app/jobs/labour_law/print_links_job.rb
@@ -4,14 +4,12 @@ class LabourLaw::PrintLinksJob < ApplicationJob
   def perform
     aml = LabourLaw::Document.find_by_document_code("aml")
     wip = aml.revisions.draft.first
-    section_headings = wip.elements.section_heading.index_order
+    section_headings = wip.elements.section_heading.index_order.where(element_chapter: "1")
     section_headings.each do |e|
-      if e.element_chapter != "1"
-        next
-      end
       path = Rails.application.routes.url_helpers.labour_law_element_path(document_slug: aml.document_slug, revision_code: wip.revision_code, element_slug: e.element_slug)
       url = "https://666a.se#{path}"
       puts "- [ ] [Chapter #{e.element_chapter} Section #{e.element_section}](#{url})"
     end
+    return nil
   end
 end
