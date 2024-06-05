4b4f3902b174f605102a4f7bbb71929cb46bc2a8
Henry Catalini Smith <henry@catalinismith.com>
2024-06-05 07:56:06 +0200

    Disable jobs pending scraper fixes

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -9,70 +9,70 @@ end
 
 scheduler = Rufus::Scheduler.singleton
 
-scheduler.cron("0 9 * * *") do
-  WorkEnvironment::MorningJob.perform_later(
-    Date.yesterday.strftime("%Y-%m-%d"),
-    cascade: true,
-    notify: true
-  )
-end
+# scheduler.cron("0 9 * * *") do
+#   WorkEnvironment::MorningJob.perform_later(
+#     Date.yesterday.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     notify: true
+#   )
+# end
 
-scheduler.cron("0 10 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    2.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 10 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     2.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
 
-scheduler.cron("0 11 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    4.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 11 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     4.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
 
-scheduler.cron("0 12 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    8.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 12 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     8.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
 
-scheduler.cron("0 13 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    16.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 13 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     16.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
 
-scheduler.cron("0 14 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    32.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 14 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     32.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
 
-scheduler.cron("0 15 * * *") do
-  WorkEnvironment::DayJob.perform_later(
-    64.days.ago.strftime("%Y-%m-%d"),
-    cascade: true,
-    force: true,
-    notify: true,
-    purge: true
-  )
-end
+# scheduler.cron("0 15 * * *") do
+#   WorkEnvironment::DayJob.perform_later(
+#     64.days.ago.strftime("%Y-%m-%d"),
+#     cascade: true,
+#     force: true,
+#     notify: true,
+#     purge: true
+#   )
+# end
