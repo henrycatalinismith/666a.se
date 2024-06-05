6c42ffb181f6f4e34f2e9c38094bf6ba8cbeee1f
Henry Catalini Smith <henry@catalinismith.com>
2024-06-05 07:48:07 +0200

    Update job

--- app/jobs/labour_law/print_links_job.rb
+++ app/jobs/labour_law/print_links_job.rb
@@ -4,7 +4,7 @@ class LabourLaw::PrintLinksJob < ApplicationJob
   def perform
     aml = LabourLaw::Document.find_by_document_code("aml")
     wip = aml.revisions.draft.first
-    section_headings = wip.elements.section_heading.index_order.where(element_chapter: "2")
+    section_headings = wip.elements.section_heading.index_order.where(element_chapter: "3")
     section_headings.each do |e|
       path = Rails.application.routes.url_helpers.labour_law_element_path(document_slug: aml.document_slug, revision_code: wip.revision_code, element_slug: e.element_slug)
       url = "https://666a.se#{path}"
