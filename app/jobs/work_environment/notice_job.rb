aee68a57e48dc0be5122f6b425ac1d4e351bba11
Henry Catalini Smith <henry@catalinismith.com>
2024-06-05 08:27:33 +0200

    More

--- app/jobs/work_environment/notice_job.rb
+++ app/jobs/work_environment/notice_job.rb
@@ -2,9 +2,10 @@ class WorkEnvironment::NoticeJob < ApplicationJob
   queue_as :default
 
   def perform(notification_id = nil, options = {})
-    users = User::Account.order(created_at: :asc).offset(2).limit(100)
+    users = User::Account.order(created_at: :asc).offset(58).limit(44)
     users.each do |user|
       NoticeMailer.with(user: user).notice_mail.deliver_now
+      sleep(1)
     end
   end
 end
