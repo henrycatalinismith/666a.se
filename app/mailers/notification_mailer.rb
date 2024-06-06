e26d61afb38dfc90988e2d0b0705f43f8431922b
Henry Catalini Smith <henry@catalinismith.com>
2024-06-06 21:48:47 +0200

    Update email templates (fixes #50)

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,7 @@ class NotificationMailer < ApplicationMailer
     @document = @notification.document
     @target = @notification.subscription.company_subscription? ? @document.company_name : @document.workplace_name
     @user = @notification.subscription.account
-    @url = "https://www.av.se/om-oss/sok-i-arbetsmiljoverkets-diarium/?id=" + @document.document_code
+    @url = "https://www.av.se/om-oss/diarium-och-allmanna-handlingar/bestall-handlingar/Case/?id=" + @document.case_code
     @unsubscribe_url = "https://666a.se/unsubscribe/#{@notification.subscription.id}"
     I18n.with_locale(@user.locale) do
       mail(
