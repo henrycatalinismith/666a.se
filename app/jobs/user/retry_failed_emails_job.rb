3c46c65ef2609205b9970f3e3b64d9d133815f57
Henry Catalini Smith <henry@catalinismith.com>
2024-06-10 21:24:57 +0200

    Add User::RetryFailedEmailsJob for #54

---
+++ app/jobs/user/retry_failed_emails_job.rb
@@ -0,0 +1,9 @@
+class User::RetryFailedEmailsJob < ApplicationJob
+  queue_as :default
+
+  def perform(since = 1.week.ago)
+    User::Notification.email_error.where("created_at > ?", since).each_with_index do |notification, i|
+      User::EmailJob.set(wait: index.seconds).perform_later(notification.id)
+    end
+  end
+end
