026e575869033affa9dccce5ec48b7aea18f7e28
Henry Catalini Smith <henry@catalinismith.com>
2024-06-10 21:56:39 +0200

    Fix variable name typo

--- app/jobs/user/retry_failed_emails_job.rb
+++ app/jobs/user/retry_failed_emails_job.rb
@@ -2,7 +2,7 @@ class User::RetryFailedEmailsJob < ApplicationJob
   queue_as :default
 
   def perform(since = 1.week.ago)
-    User::Notification.email_error.where("created_at > ?", since).each_with_index do |notification, i|
+    User::Notification.email_error.where("created_at > ?", since).each_with_index do |notification, index|
       User::EmailJob.set(wait: index.seconds).perform_later(notification.id)
     end
   end
