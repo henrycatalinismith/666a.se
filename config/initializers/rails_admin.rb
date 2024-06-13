237e0813d4d2747db697471b4366a1f0f4411ae1
Henry Catalini Smith <henry@catalinismith.com>
2024-06-13 14:54:18 +0200

    Sort notifications reverse chronologically

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,30 +1,20 @@
 RailsAdmin.config do |config|
+  config.default_items_per_page = 64
   config.main_app_name = ["666a"]
   config.asset_source = :importmap
   config.parent_controller = "::AdminActionController"
 
-  ### Popular gems integration
-
-  ## == Devise ==
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
   config.current_user_method(&:current_user)
-
-  ## == CancanCan ==
   config.authorize_with :cancancan
 
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/railsadminteam/rails_admin/wiki/Base-configuration
-
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  config.model "User::Notification" do
+    list do
+      sort_by :created_at
+    end
+  end
 
   config.actions do
     dashboard                     # mandatory
