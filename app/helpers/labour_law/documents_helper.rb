a7652b49bae14d68e4ed559f73b3e40e2b9c85a2
Henry Catalini Smith <henry@catalinismith.com>
2024-06-25 22:00:11 +0200

    Replace hardcoded subtitle with dynamic one

--- app/helpers/labour_law/documents_helper.rb
+++ app/helpers/labour_law/documents_helper.rb
@@ -1,15 +1,2 @@
 module LabourLaw::DocumentsHelper
-  def legal_document_subtitle(document, revision)
-    if document.document_code == "aml"
-      "Version #{revision.revision_code}, English translation"
-    elsif document.document_code == "mbl"
-      "Version 2021:1114, English translation"
-    elsif document.document_code == "las"
-      "Version 2022:836, English translation"
-    elsif document.document_code == "atl"
-      "Version #{revision.revision_code}, English translation"
-    else
-      ""
-    end
-  end
 end
