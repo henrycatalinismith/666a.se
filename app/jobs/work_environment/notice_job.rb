723c0d1e3adfc6fcf5c72385ac033df4b5c0805a
Henry Catalini Smith <henry@catalinismith.com>
2025-01-13 20:19:22 +0100

    Final mailout snet

--- app/jobs/work_environment/notice_job.rb
+++ app/jobs/work_environment/notice_job.rb
@@ -2,8 +2,9 @@ class WorkEnvironment::NoticeJob < ApplicationJob
   queue_as :default
 
   def perform(notification_id = nil, options = {})
-    users = User::Account.order(created_at: :asc).offset(0).limit(1)
+    users = User::Account.order(created_at: :asc)
     users.each do |user|
+      puts user.email
       NoticeMailer.with(user: user).notice_mail.deliver_now
       sleep(1)
     end
